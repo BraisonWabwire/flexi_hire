b882181aa7f58bb04bee2c13a583491befb84a27
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-16 16:50:16 +0300

    Starting designing the homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,20 +13,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            GestureDetector(
-              onTap: () {
-              // Do something
-              },
-              child: Icon(Icons.arrow_back),
-            ),
-          ],
-        ),
-      ),
+    return Container(
+      color: Colors.white,
+      child: Scaffold(appBar: AppBar(title: Text(
+                  "Welcome",
+                  style:  GoogleFonts.montserrat(
+                  fontSize: 20,
+                  letterSpacing: 1.1,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),)),
     );
   }
 }
