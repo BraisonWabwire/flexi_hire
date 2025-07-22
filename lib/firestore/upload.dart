4247c4ce9a1248f8ec7736184ab8bdc066b4637b
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-22 22:11:40 +0300

    Creating jobs model

--- lib/firestore/upload.dart
+++ lib/firestore/upload.dart
@@ -1,1 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Job {
+  final String category;
+  final String title;
+  final List<String> specs;
+  final String applyLink;
+
+  Job({
+    required this.category,
+    required this.title,
+    required this.specs,
+    required this.applyLink,
+  })
+}
